46f61ba07abff85f6092664bd7186c225bab6088
ShababKabab <sababsababsabab12345@gmail.com>
2024-03-08 20:22:57 +0600

    added new postgresql

--- src/main/java/com/zosh/model/User.java
+++ src/main/java/com/zosh/model/User.java
@@ -17,10 +17,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 //@Data
+@Table(name = "\"user\"")
 public class User {
 
 	@Id
